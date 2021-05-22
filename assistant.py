3dfc9ec76a38899c8a42f9c4dcc870c83d7aea10
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-05-22 16:51:23 +0530

    date time function added

--- assistant.py
+++ assistant.py
@@ -1,4 +1,5 @@
 import pyttsx3 # text-to-speech module
+import datetime
 
 engine = pyttsx3.init() # initialisation
 # voice selection
@@ -13,4 +14,13 @@ def speak(message):
     engine.say(message)
     engine.runAndWait()
 
-speak('hello there')
+def time():
+    Time = datetime.datetime.now().strftime("%I:%p:%M 'minutes and' %S 'seconds")
+    speak('The present time is {}'.format(Time))
+
+def date():
+    year = str(datetime.datetime.now().year)
+    m = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    month = m[int(datetime.datetime.now().month)]
+    date = str(datetime.datetime.now().day)
+    speak('Today\'s date is {} {} {}'.format(month,date,year))
