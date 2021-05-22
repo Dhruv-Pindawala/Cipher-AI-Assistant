b26cfd3ccd62b6f6b0700cf6128c90cd50d9b9b7
Dhruv-Pindawala <dhruv.pindawala@gmail.com>
2021-05-22 16:03:40 +0530

    voice and speech rate selection

--- assistant.py
+++ assistant.py
@@ -1,9 +1,16 @@
 import pyttsx3 # text-to-speech module
 
 engine = pyttsx3.init() # initialisation
+# voice selection
+voices = engine.getProperty('voices') # obtain voice properties from the module
+engine.setProperty('voice', voices[1].id) # [1] for female voice and [0] for male voice. 0 to 5, total 6 different voices.
+# speed selection
+newVoiceRate = 140 # words per minute
+engine.setProperty('rate', newVoiceRate)
+
 
 def speak(message):
     engine.say(message)
     engine.runAndWait()
 
-speak("hey fellas,how may i help you?")
+speak('hello there')
